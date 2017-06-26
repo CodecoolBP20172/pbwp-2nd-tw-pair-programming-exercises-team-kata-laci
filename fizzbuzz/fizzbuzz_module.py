460ea79332615b847f9ea32c159577a3408c4349
Laszlo Kiss <lklaszlokiss@gmail.com>
2017-06-26 14:14:11 +0200

    We are GODS on Fizzbuzz

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,9 +1,21 @@
-def fizzbuzz(number):
-    return
+
+
+def fizzbuzz(i):
+    if i % 15 == 0:
+        return  "FizzBuzz"
+    elif i % 3 == 0:
+        return  "Fizz"
+    elif i % 5 == 0:
+        return  "Buzz"
+    else:
+        return  i
+
 
 
 def main():
-    return
+    for i in range(1,101):
+        print(fizzbuzz(i))
+    #return
 
 if __name__ == '__main__':
     main()
